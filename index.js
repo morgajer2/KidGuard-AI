d43dd728da942fc5e16d2eedd946d9dcab35fd90
Mor Gajer <morgajer2@gmail.com>
2022-05-26 11:10:02 +0300

    text recognition + ai -> working togheter

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import * as toxicity from '@tensorflow-models/toxicity';
-import  express from 'express';
+import express from 'express';
 import cors from 'cors';
 const app = express();
 
+import Tesseract from 'tesseract.js';
 
 
 // ADD THIS
@@ -11,45 +12,56 @@ app.use(cors());
 
 
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+    console.log('Example app listening on port 3000!'),
 );
 
 app.get('/AI/', (req, res) => {
 
-    var sentence = req.originalUrl.split('=')[1].replace('+',' ');
-    console.log(sentence);
+    //get image url from request
+    var imageUrl = req.originalUrl.split('=')[1].replaceAll('%2F','/');
+    console.log(imageUrl);
 
-    console.log("AI");
-    // The minimum prediction confidence.
-    const threshold = 0.9;
+    //get text from the image
+    Tesseract.recognize(
+        imageUrl,
+        'eng',
+        { logger: m => console.log(m) }
+    ).then(({ data: { text } }) => {
+        console.log(text);
 
-    // Which toxicity labels to return.
-    const labelsToInclude = ['toxicity','severe_toxicity','identity_attack', 'insult', 'threat','sexual_explicit','obscene'];
-    console.log("yarin");
-    var ans = false;
+        console.log("AI");
+        // The minimum prediction confidence.
+        const threshold = 0.9;
 
-    toxicity.load(threshold, labelsToInclude).then(model => {
-        // Now you can use the `model` object to label sentences. 
-        model.classify([sentence]).then(predictions => 
-            {
-                
-                for(var i=0;i<predictions.length;i++)
-                {
+        // Which toxicity labels to return.
+        const labelsToInclude = ['toxicity', 'severe_toxicity', 'identity_attack', 'insult', 'threat', 'sexual_explicit', 'obscene'];
+        var ans = false;
+
+        //checking if the words are bad
+        toxicity.load(threshold, labelsToInclude).then(model => {
+            model.classify([text]).then(predictions => {
+
+                for (var i = 0; i < predictions.length; i++) {
                     console.log(predictions[i].results[0].match);
-                    if(predictions[i].results[0].match==true){
-                    ans = true;
-                    break;
+                    if (predictions[i].results[0].match == true) {
+                        ans = true;
+                        break;
                     }
                 }
                 res.send(ans);
 
             })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }).catch(function (error) {
-        console.log("load "+error);
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }).catch(function (error) {
+            console.log("load " + error);
+        });
     });
+
+
+
+
 
 
 });
